2ace2ceb09a1b6db7f14f90a044cb0d5469b3ace
EpeTukone <fren.ilya@tut.by>
2015-11-28 12:30:41 +0300

    task №1

---
+++ valuta.py
@@ -0,0 +1,4 @@
+s = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%3D%22USDBYR%2CEURBYR%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
+import json
+parsed_data = json.loads(parsed_data)
+print persed_data
